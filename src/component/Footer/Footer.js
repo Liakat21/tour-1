9f0518edda26c5a73341ce82e3ccbc5448e5ed02
Liakat21 <jupiterali1990@gmail.com>
2022-03-02 20:09:48 +0600

    margin set

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -4,17 +4,18 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <div className="footer bg-primary text-white ">
-            <div className="container my-4 d-flex col col-md-4 col-lg-6">
-                <div className="row row-col">
+        <div className="footer bg-primary text-white  ">
+            <div className="container my-4 d-flex col col-md-4 col-lg-6 ">
+                <div className="column row row-col  ">
 
-                    <div className=" col-md-4 col">
+                    <div className='d-flex'>
+                    <div className="column col-md-4 col">
                         <h1>Our Vision :</h1>
                         <h4> To be a reluxable jurney</h4>
                         <p>One always quickly consider that for the developmental <br /> role of education in a country the tertiary level is important.</p>
                         <p>email : tourpackage@gmail.com</p>
                     </div>
-                    <div className="col-md-4 col">
+                    <div className="column  col-md-4 col">
                         <h1>Contact Info :</h1>
                         <h4> Our Address</h4>
                         <p>7A, 89/2, Haque chamber,
@@ -28,9 +29,10 @@ const Footer = () => {
                             info@shonod.com</p>
                         <p>email : tourpackage@gmail.com</p>
                     </div>
-                    <div className="col-md-4 col">
+                    <div className=" column  col-md-4 col">
                         <h3>Payment :</h3>
                         <img className="payment" src="https://i2.wp.com/thereviewstories.com/wp-content/uploads/2020/10/paymentgateway.jpg?w=1080&ssl=1" alt="" />
+                    </div>
                     </div>
                 </div>
 
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
 
     return (
         <div>
-            <Carousel className=" carousel container my-4">
+            <Carousel className=" carousel  my-4">
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
